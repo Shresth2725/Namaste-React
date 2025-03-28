d03eb36ab230f0b06ce3d288132e46b811e555c4
Shresth <2725shresth@gmail.com>
2025-03-28 17:50:34 +0530

    Episode 3

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 
-/* 
-    <div class = "parent">
-        <div class = "child">
-            <h1 class = "h11">Hello</h1>
-            <h1 class = "h12">React</h1>
-        </div>
-    </div>
-*/
 
-const parent = React.createElement(
-    "div",
-    { className: "parent" },
-    React.createElement(
-        "div",
-        { className: "child"},
-        React.createElement("h1" , { className: "child"} , "Hello")
-            // React.createElement("h1" , { className: "child"} , "React")
-            
-    )
+// React Element
+// const parent = React.createElement(
+//     "h1",
+//     {},
+//     "Hello, world!"
+// )
+
+// const parentjsx = <h1 id="heading" className="head">Hello, World</h1>
+
+// Functional React Component
+// const HeadingComponent = () => {
+//     return <h1>Namaste React</h1>;
+// }
+
+// const HeadingComponent1 = () => <h1>Namaste React</h1>;
+
+const Title = () => (
+    <h1>HEllo</h1>
+);
+
+const HeadingComponent = () => (
+    <div>
+        <h1>Heading</h1>
+        <Title />
+    </div>
 )
 
+
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
